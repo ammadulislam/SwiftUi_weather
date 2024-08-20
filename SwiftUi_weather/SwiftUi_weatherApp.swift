e63752406b3337e27c6a3bc6155c794fa32b74b8
Zafran zaman <mzafransultani56@gmal.com>
2024-08-20 21:55:21 +0500

    Initial Commit

---
+++ SwiftUi_weather/SwiftUi_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_weatherApp.swift
+//  SwiftUi_weather
+//
+//  Created by Zafran Mac on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
